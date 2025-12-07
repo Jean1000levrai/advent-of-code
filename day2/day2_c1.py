996fede003b900265508b52959f0aa5a01d49c67
Jean Salomon <jean2salomon@icloud.com>
2025-12-07 22:07:44 +0100

    this is akward, misread the instructions

--- day2/day2_c1.py
+++ day2/day2_c1.py
@@ -3,30 +3,57 @@ def load_products(file: str) -> list[str]:
         content = f.read()
         return [word.strip() for word in content.split(',')]
 
+def valid_debug(s: str) -> str:
+    n = len(s)
+    good = True
+    for i in range(1, n//2 + 1):
+        print(f"iteration {i}", f"n % {i} = ", n%i)
+        if n % i == 0:
+            print("  loops : ", n//i,";i = ", i)
+            # if n//i == 2:
+            #     print(f"    {s[:i]} == {s[i:]}")
+            #     if s[:i] == s[i:]:
+            #         return s
+            for j in range(2, (n//i) + 1):
+                print(f"    {s[:i]} == {s[i*(j-1):i*j]}")
+                if s[:i] == s[i*(j-1):i*j]:
+                    print("      good")
+                    good = True
+                else:
+                    print("      break")
+                    good = False
+                    break
+            if good:
+                return s
+    return '0'
+
 def valid(s: str) -> str:
     n = len(s)
-
-    pi = [0] * n
-    j = 0
-    for i in range(1, n):
-        while j > 0 and s[i] != s[j]:
-            j = pi[j - 1]
-        if s[i] == s[j]:
-            j += 1
-        pi[i] = j
-
-    p = pi[-1]
-
-    if p > 0 and n % (n - p) == 0:
-        return s[:n - p]
-    else:
-        return 0
+    good = True
+    for i in range(1, n//2 + 1):
+        if n % i == 0:
+            if n//i == 2:
+                if s[:i] == s[i:]:
+                    return s
+            for j in range(2, (n//i) +1):
+                if s[:i] == s[i*(j-1):i*j]:
+                    good = True
+                else:
+                    good = False
+                    break
+            if good:
+                return s
+    return '0'
 
 def check_one_range(range_product: str, code: int) -> int:
     dash = range_product.index('-')
     left = range_product[:dash]
     right = range_product[dash+1:]
+    print(range_product)
     for i in range(int(left), int(right)):
+        print("  ", i)
+        if valid(str(i)) != '0':
+            print("---    ",valid(str(i)))
         code = code + int(valid(str(i)))
     return code
 
@@ -37,5 +64,19 @@ def main() -> None:
         code = code + check_one_range(prod, code)
     print(code)
 
+def main_test():
+    products = load_products("test.txt")
+    code = 0
+    print(products)
+    for prod in products:
+        code = code + check_one_range(prod, code)
+    print(code)
+
+def test():
+    a = "1188511880-1188511890"
+    print(valid_debug("1188511880"))
+    print(valid_debug("1188511885"))
+
 if __name__ == "__main__":
-    main()
+    main_test()
+    # test()
